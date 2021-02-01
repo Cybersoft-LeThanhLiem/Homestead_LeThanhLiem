dcfdeb76408d1d5e299d5709a7995d24ad6e0840
Liem <leliem28@gmail.com>
2021-02-01 11:50:43 +0700

    Add Back To Top button

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,18 @@ $('.counter').countUp({
     'time': 2000,
     'delay': 10
 });
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
