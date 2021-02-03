4df005d74c1f7a74d564e83d5fc3b7b88dc8ba45
Liem <leliem28@gmail.com>
2021-02-04 06:19:40 +0700

    Fix Back To Top button

--- js/scripts.js
+++ js/scripts.js
@@ -33,18 +33,3 @@ $('.counter').countUp({
     'time': 2000,
     'delay': 10
 });
-
-
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('#backToTop').fadeIn();
-        } else {
-            $('#backToTop').fadeOut();
-        }
-    });
-    $('#backToTop').click(function () {
-        $("html, body").animate({ scrollTop: 0 }, 600);
-        return false;
-    });
-});
